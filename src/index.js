e7fe154cc5229d7ec4fc4e8880fd61c8b11ea2b3
ottonsam <ottonsam@gmail.com>
2021-10-31 05:09:32 -0300

    Create find user function

--- src/index.js
+++ src/index.js
@@ -11,14 +11,24 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({ error: 'User not found!'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
   const userAlreadyExists = users.some(
-    (user) => user.username == username
+    (user) => user.username === username
   );
 
   if (userAlreadyExists) {
